d9362e6dde1c192df625c5a5b5f86318ccc88fea
Jayson Poirier <2243405@cegeplimoilou.ca>
2023-03-03 10:13:57 -0500

    java doc VecteurFormes

--- src/formes/VecteurFormes.java
+++ src/formes/VecteurFormes.java
@@ -24,10 +24,18 @@ public class VecteurFormes {
         vecteur.add(forme);
     }
 
+    /**
+     * Obtenir le vecteur de formes
+     *
+     * @return le vecteur de formes
+     */
     public ArrayList<Forme> getVecteur() {
         return vecteur;
     }
 
+    /**
+     * Mélanger les éléments du vecteur.
+     */
     public void melanger() {
 
     }
@@ -36,8 +44,23 @@ public class VecteurFormes {
 
     }
 
-    public void remplir(int nbrFormesVoulues) throws FormeException {
-        while (vecteur.size() < nbrFormesVoulues) {
+    /**
+     * Mettre le nombre de formes voulues dans le vecteur. Les formes qui seront
+     * placées dans le vecteur devront être générées de la façon suivante:
+     * <p>
+     * Tant que le nombre de formes souhaitées n'est pas atteint on crée une forme
+     * à la fois, pour toutes les formes disponibles (Cercle, Rectangle et
+     * Triangle) combiné à toutes les couleurs possibles (rouge, vert, bleu,
+     * jaune, noir, orange). Quand toutes les combinaisons sont épuisées on
+     * recommence.
+     * <p>
+     * Le but final étant d'obtenir le bon nombre de formes, les plus
+     * diversifiées possibles.
+     *
+     * @param nbrElements le nombre de formes voulues
+     */
+    public void remplir(int nbrElements) throws FormeException {
+        while (vecteur.size() < nbrElements) {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 6; j++) {
                     if (i == 0) {
@@ -65,6 +88,10 @@ public class VecteurFormes {
         }
     }
 
+    /**
+     * Trier en ordre croissant le vecteur de formes en utilisant le "compareTo"
+     * disponible sur les objets.
+     */
     public void trier() {
 
     }
@@ -79,4 +106,4 @@ public class VecteurFormes {
     }
 
 }
-}
+
