41e5809fe067436327c83c169be63306144feb1c
JB <2236326@cegeplimoilou.ca>
2023-03-03 10:11:34 -0500

    java doc de l'interface dans JeuMemoire

--- src/jeu/JeuMemoire.java
+++ src/jeu/JeuMemoire.java
@@ -50,11 +50,24 @@ public class JeuMemoire implements IJeuMemoire {
         return grilleDeJeu;
     }
 
+    /**
+     * Connaître le niveau du jeu.
+     *
+     * @return le niveau du jeu
+     */
     @Override
     public int getNiveau() {
         return niveau;
     }
 
+    /**
+     * Obtenir sous forme de chaîne sans espace le nom et la couleur de la forme
+     * à l'emplacement désiré dans la grille.
+     *
+     * @param ligne   coordonnée ligne de la matrice grille
+     * @param colonne coordonnée colonne de la matrice grille
+     * @return nom et couleur concaténée en chaîne de caractère
+     */
     @Override
     public String getNomForme(int ligne, int colonne) {
         return grilleDeJeu[colonne][ligne].getNom() + grilleDeJeu[colonne][ligne].getCouleur();
@@ -64,11 +77,35 @@ public class JeuMemoire implements IJeuMemoire {
         return vecteurFormes;
     }
 
+    /**
+     * Valide si la coordonnée jouée par le joueur humain est valide et dans
+     * l'ordre selon les coordonnées générées par l'ordinateur. Voir la méthode
+     * "jouerOrdi()"
+     *
+     * @param ligne   coordonnée ligne de la grille
+     * @param colonne coordonnée colonne dans la grille
+     * @return oui ou non si la coordonnée du joueur est la coordonnée jouée par
+     * l'ordi dans l'ordre respecté.
+     */
     @Override
     public boolean jouerHumain(int ligne, int colonne) {
 
     }
 
+    /**
+     * L'intelligence du jeu. Génère un tableau de coordonnées (des objets <b>Point(colonne, ligne)</b>)
+     * au hasard. Les points générés doivent être valides dans la grille de jeu.
+     * Le nombre de points générés est en relation avec le niveau courant du jeu.
+     * Il obéit à la règle suivante:
+     * <p>
+     * Le nombre de points générés = niveau courant du jeu + 2
+     *
+     * <b>Il est important qu'un même point ne soit pas choisi 2 fois.</b>
+     * <b>Note</b>: la classe java.awt.Point encapsule un x et un y. Alors que dans le JeuMemoire,
+     * on utilise plutôt colonne et ligne. Ainsi, x correspond à colonne et y à ligne.
+     *
+     * @return la liste des coordonnées <b>Point(colonne, ligne)</b> des formes choisies dans la grille.
+     */
     @Override
     public ArrayList<Point> jouerOrdi() {
         for (int i = 0; i < (niveau + 2); i++) {
@@ -93,6 +130,9 @@ public class JeuMemoire implements IJeuMemoire {
         vecteurFormes.melanger();
     }
 
+    /**
+     * Ajoute 1 au niveau si celui-ci est en-dessous de 6, autrement il demeure à 6.
+     */
     @Override
     public void setNiveauPlusUn() {
         if (niveau < NIVEAU_MAX) {
