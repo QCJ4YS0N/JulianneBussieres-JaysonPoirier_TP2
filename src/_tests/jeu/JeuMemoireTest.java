284fa65724f74fb8993d450f1a6f6fc966c603e3
Julianne Bussieres <2236326@cegeplimoilou.ca>
2023-03-05 20:27:23 -0500

    création de la classe de test JeuMemoireTest

---
+++ src/_tests/jeu/JeuMemoireTest.java
@@ -0,0 +1,30 @@
+package jeu;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class JeuMemoireTest {
+    @Test
+    void getGrille() {
+    }
+
+    @Test
+    void getNiveau() {
+    }
+
+    @Test
+    void getNomForme() {
+    }
+
+    @Test
+    void getVecteur() {
+    }
+
+    @Test
+    void jouerHumain() {
+    }
+
+    @Test
+    void jouerOrdi() {
+    }
+}
